ebbf9cdae7eb257a14166c6de376bdf8b1ccb4c0
Tarroza <aeron.tarroza@accenture.com>
2024-11-08 18:34:07 +0800

    remove procfile

--- app.py
+++ app.py
@@ -499,4 +499,5 @@ def format_response(response_text):
     return "".join(formatted_lines)
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Default to 5000 if no PORT is found
+    app.run(debug=False, host='0.0.0.0', port=port)
